2045e080dd0d25de30ff8e56b18c421614778d52
chandra <chandra@anterin.id>
2021-02-23 11:34:12 +0700

    first commit

---
+++ src/utils/color.js
@@ -0,0 +1,9 @@
+export default  color = {
+    green : "#739c3e",
+    lightGreen : "#aec55d",
+    black  : "#151515",
+    brown : "#2f1e0a",
+    lightBrown : "#dcd294",
+    secondary : "#e9f0c0",
+    white : "#ffffff",
+}
